87c2f57dcddfa2e4a199e5bce4ce6e69f33fc857
dixitSankalp <sankalpry07@gmail.com>
2025-08-16 15:25:33 +0530

    33429

--- home/views.py
+++ home/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-from account.models import Restaurant  
 
 def homepage(request):
-    restaurant_name = None
-    restaurant = Restaurant.objects.first()
-    if restaurant:
-        restaurant_name = restaurant.name
-    
-    return render(request, 'home.html', {'restaurant_name': restaurant_name})
+    restaurant_name = "My Awesome Restaurant"  # or fetch from model/settings
+    return render(request, "home/homepage.html", {"restaurant_name": restaurant_name})
