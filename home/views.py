7c72d791c8db756f973aafe9f6c46b8f28db1144
dixitSankalp <sankalpry07@gmail.com>
2025-08-16 15:40:21 +0530

    33428

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
+from django.conf import settings
 from django.shortcuts import render
 
 def homepage(request):
-    restaurant_name = "My Awesome Restaurant"  # or fetch from model/settings
-    return render(request, "home/homepage.html", {"restaurant_name": restaurant_name})
+    restaurant_name = settings.RESTAURANT_NAME
+    restaurant_phone = settings.RESTAURANT_PHONE
+    return render(request, "home/homepage.html", {
+        "restaurant_name": restaurant_name,
+        "restaurant_phone": restaurant_phone,
+    })
