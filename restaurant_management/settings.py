5e0ab2b21da6a646f7fa2ef3caa1070722bdb9a3
dixitSankalp <sankalpry07@gmail.com>
2025-08-16 15:38:59 +0530

    33428

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -130,3 +130,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+RESTAURANT_NAME = "My Awesome Restaurant"
+RESTAURANT_PHONE = "+91 9876543210"
