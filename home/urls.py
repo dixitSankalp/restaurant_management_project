78e049cdf03ecf9aef94753e40e7f685262e424c
dixitSankalp <sankalpry07@gmail.com>
2025-08-18 17:32:57 +0530

    33425

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('menu/', views.menu_list, name='menu'),  # 👈 new route
 ]
